656d390b858b544504b2855fb9b63a0c1f27a017
Gaetan <Gaetan@172.18.109.151>
2020-01-10 09:16:31 +0100

    new classes pro, client, counsellor, accounts not finished

--- src/java/Model/User.java
+++ src/java/Model/User.java
@@ -10,5 +10,29 @@ package Model;
  * @author Gaetan
  */
 public class User {
+    private String login;
+    private String mdp;
+    
+    User(String login, String mdp){
+        this.login= login;
+        this.mdp=mdp;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+    
     
 }
