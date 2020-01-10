2ecd306cd6af84236924f40bb027ca7a68e2895e
Gaetan <Gaetan@172.18.109.151>
2020-01-10 10:11:24 +0100

    create saving account

---
+++ src/java/Model/Saving.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Gaetan
+ */
+public class Saving extends Accounts{
+    
+    Saving(double credits, double account_id){
+        super(credits, account_id);
+    }
+        
+}
