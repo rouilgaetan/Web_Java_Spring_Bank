d4e900825420f6af41cf00310d60c44b9e492e90
croux08 <croux08@BE13-19.sfasp2mi.univ-poitiers.fr>
2020-01-10 11:03:19 +0100

    cryptage mdp

--- src/java/controllers/ConnexionController.java
+++ src/java/controllers/ConnexionController.java
@@ -5,10 +5,16 @@
  */
 package controllers;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.portlet.mvc.AbstractController;
+import org.springframework.web.servlet.ModelAndView;
+import static sun.security.krb5.Confounder.bytes;
 
 /**
  *
@@ -20,5 +26,24 @@ public class ConnexionController extends AbstractController{
     @RequestMapping(value="index", method=RequestMethod.GET)
     public String init(){
         return "index";
+    }
+    
+    @RequestMapping(value="hello", method=RequestMethod.POST)
+    public ModelAndView handleRequestInternal(
+    HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView mv = new ModelAndView("message");
+        String nom = request.getParameter("nom");
+        String password = request.getParameter("password");
+        
+        //Cryptage mot de passe
+        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+        crypt.reset();
+        crypt.update(password.getBytes("UTF-8"));
+        String mdpcrypt =  new BigInteger(1, crypt.digest()).toString(16);
+        
+        
+        mv.addObject("nom",nom);
+        mv.addObject("password", mdpcrypt);
+        return mv;
     }
 }
