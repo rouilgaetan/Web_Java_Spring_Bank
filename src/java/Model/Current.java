15b406d2310016abbe0689d0a5fbe868ccf2e3ff
Gaetan <Gaetan@172.18.109.151>
2020-01-10 10:11:13 +0100

    create current account

---
+++ src/java/Model/Current.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Gaetan
+ */
+public class Current extends Accounts{
+    
+    Current(double credits, double account_id){
+        super(credits, account_id);
+    }
+}
