6bcdcc7504718805141567fb46b37278d62097bd
croux08 <croux08@BE11-01.sfasp2mi.univ-poitiers.fr>
2020-02-13 12:09:17 +0100

    js appliqué

--- web/js/formInscr.js
+++ web/js/formInscr.js
@@ -1,26 +1,17 @@
-function myfunct(){
-    document.querySelector("#divPro").setAttribute(test, none);
-};
-
-/*
-$(document).ready(function(){
-    console.log( "ready!" );
-    alert('rouge');
+document.addEventListener("DOMContentLoaded", function() { 
+    if(document.querySelector("#pro")!==null){
+        if(document.querySelector("#particulier")!==null){
+            if(document.querySelector("#divPro")!==null){
+                document.querySelector("#divPro").style.display="none";
+                var radio_pro = document.querySelector("#pro");
+                var radio_part = document.querySelector("#particulier");
+                radio_pro.addEventListener('click', function(){
+                    document.querySelector("#divPro").style.display="block";
+                });
+                radio_part.addEventListener('click', function(){
+                    document.querySelector("#divPro").style.display="none";
+                });
+            }
+        }
+    }
 });
-
-$('#pass').val('abcde');
-alert('rouge');*/
-/*
-(function() {
-    $('#pass').val('abcde');
-})();*/
-
-/*
-if($('input[name=type]:checked').val()==="pro"){
-    document.getElementById(divPro).style.display = none;
-}else{
-    document.getElementById(divPro).style.display = block;
-}
-
-document.querySelector('div.test').style.color = 'blue';
-document.querySelector('div.test').textContent = 'blue';*/
